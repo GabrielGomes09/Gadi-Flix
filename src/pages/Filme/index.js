f89cc24e97ed706dfdd7d95e7928c14375a64c00
GabrielGomes09 <gadigomes09@gmail.com>
2026-04-06 11:05:16 -0300

    Página de detalhes dos filmes

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,74 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-function Filme(){
-    return(
-        <div>
-            <h1>DETALHES DO FILME</h1>
-        </div>
-    )
+import api from "../../services/api";
+import "./Filme.css";
+
+function Filme() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "7883136f0ca1dedfe18a2d3c9e3290cb",
+            language: "pt-BR",
+          },
+        })
+        .then((Response) => {
+          setFilme(Response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("Filme não encontrado");
+          navigate("/", { replace: true });
+          return;
+        });
+    }
+    loadFilme();
+
+    return () => {
+      console.log("componente desmontado");
+    };
+  }, [navigate, id]);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Caaregando detalhes...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a
+            href={`https://www.youtube.com/results?search_query=${encodeURIComponent(filme.title + " Trailer")}`}
+            target="_blank"
+            rel="external"
+          >
+            Trailer
+          </a>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Filme;
